e094e75e92872893d68de4f05f34b57dbc6bacec
Charles Archibong <charlesarchibong10@gmail.com>
2020-06-09 21:05:17 +0100

    integrated fcm

---
+++ lib/helpers/notification.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class Notification {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  Future<String> getToken() async {
+    String token = await _firebaseMessaging.getToken();
+    return token;
+  }
+// _firebaseMessaging.configure(
+//     onLaunch: (Map<String, dynamic> message) {
+//       print('onLaunch called');
+//     },
+//     onResume: (Map<String, dynamic> message) {
+//       print('onResume called');
+//     },
+//     onMessage: (Map<String, dynamic> message) {
+//       print('onMessage called');
+//     },
+//   );
+//   _firebaseMessaging.subscribeToTopic('all');
+//   _firebaseMessaging.requestNotificationPermissions(IosNotificationSettings(
+//     sound: true,
+//     badge: true,
+//     alert: true,
+//   ));
+//   _firebaseMessaging.onIosSettingsRegistered
+//       .listen((IosNotificationSettings settings) {
+//     print('Hello');
+//   });
+
+// }
+}
