b685e04304a17df0fa21b85d3ecc69dfcd66a98c
Charles Archibong <charlesarchibong10@gmail.com>
2020-07-19 21:47:19 +0100

    fixing bug

--- lib/modules/artisan/service/artisan.dart
+++ lib/modules/artisan/service/artisan.dart
@@ -147,48 +147,45 @@ class ArtisanApi extends ArtisanService {
   }
 
   @override
-  Future<List<ServiceRequest>> getMyServiceRequest() {
-    @override
-    Future<List<ServiceRequest>> getServiceRequest() async {
-      try {
-        final user = await Utils.getUserSession();
-        final String apiKey = await Utils.getApiKey();
-        String url = 'https://manager.fixme.ng/my-requested-services';
-        Map<String, String> headers = {'Authorization': 'Bearer $apiKey'};
-        Map<String, String> body = {
-          'mobile': user.phoneNumber,
-        };
-        final response = await NetworkService().post(
-          url: url,
-          body: body,
-          contentType: ContentType.URL_ENCODED,
-          headers: headers,
+  Future<List<ServiceRequest>> getMyServiceRequest() async {
+    try {
+      final user = await Utils.getUserSession();
+      final String apiKey = await Utils.getApiKey();
+      String url = 'https://manager.fixme.ng/my-requested-services';
+      Map<String, String> headers = {'Authorization': 'Bearer $apiKey'};
+      Map<String, String> body = {
+        'mobile': user.phoneNumber,
+      };
+      final response = await NetworkService().post(
+        url: url,
+        body: body,
+        contentType: ContentType.URL_ENCODED,
+        headers: headers,
+      );
+      print(response.data.toString());
+      if (response.statusCode == 200 && response.data['reqRes'] == 'true') {
+        List projects = response.data['serivceRequest'] as List;
+        if (projects.length <= 0) {
+          throw Exception('No approved bids at the moment');
+        }
+        List<ServiceRequest> bids = List();
+        if (projects.length > 0) {
+          for (var i = 0; i < projects.length; i++) {
+            ServiceRequest bid = ServiceRequest.fromMap(projects[i]);
+            bids.add(bid);
+          }
+        }
+        return bids;
+      } else {
+        throw Exception(
+          'Request was not successful, Please try again!',
         );
-        print(response.data.toString());
-        if (response.statusCode == 200 && response.data['reqRes'] == 'true') {
-          List projects = response.data['serivceRequest'] as List;
-          if (projects.length <= 0) {
-            throw Exception('No approved bids at the moment');
-          }
-          List<ServiceRequest> bids = List();
-          if (projects.length > 0) {
-            for (var i = 0; i < projects.length; i++) {
-              ServiceRequest bid = ServiceRequest.fromMap(projects[i]);
-              bids.add(bid);
-            }
-          }
-          return bids;
-        } else {
-          throw Exception(
-            'Request was not successful, Please try again!',
-          );
-        }
-      } catch (e) {
-        if (e is DioError) {
-          print(e.message);
-        }
-        rethrow;
       }
+    } catch (e) {
+      if (e is DioError) {
+        print(e.message);
+      }
+      rethrow;
     }
   }
 }
