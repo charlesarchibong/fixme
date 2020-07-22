7a38af5c83a2e22e3aaef133c35006c94fbe0284
Charles Archibong <charlesarchibong10@gmail.com>
2020-07-22 23:56:35 +0200

    consumed api to rate artisan

--- lib/modules/rate_review/provider/rate_review_provider.dart
+++ lib/modules/rate_review/provider/rate_review_provider.dart
@@ -1,12 +1,41 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:logger/logger.dart';
 import 'package:quickfix/models/failure.dart';
+import 'package:quickfix/modules/rate_review/model/rate.dart';
+import 'package:quickfix/modules/rate_review/service/rate_artisan.dart';
+import 'package:quickfix/util/Utils.dart';
 
 class RateReviewProvider with ChangeNotifier {
   bool _loading = false;
   bool get loading => _loading;
 
   Future<Either<Failure, bool>> rateArtisan(
-      String serviceId, String artisanPhone, int rating,
-      [int jobId]) {}
+      int serviceId, String artisanPhone, int rating,
+      [int jobId]) async {
+    try {
+      final user = await Utils.getUserSession();
+      final rateArtisan = RateArtisan(
+        artisanMobile: artisanPhone,
+        jobId: jobId,
+        rating: rating,
+        serviceRequestId: serviceId,
+        ratedBy: user.phoneNumber,
+      );
+
+      final rated = await RateArtisanApi().rateArtisan(rateArtisan);
+      if (rated == true) {
+        return right(rated);
+      }
+      return left(
+        Failure(message: 'Artisan was not rated, please try again'),
+      );
+    } catch (e) {
+      Logger().e(e.toString());
+      return left(
+        Failure(
+            message: 'Unable to rate artisan at the moment, please try again'),
+      );
+    }
+  }
 }
