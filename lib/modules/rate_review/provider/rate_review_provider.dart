337f3653272f68ab33d187b5fcef7cafc90d08c8
Charles Archibong <charlesarchibong10@gmail.com>
2020-07-22 11:45:07 +0200

    rate artisan method has been declay

--- lib/modules/rate_review/provider/rate_review_provider.dart
+++ lib/modules/rate_review/provider/rate_review_provider.dart
@@ -1,6 +1,12 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:quickfix/models/failure.dart';
 
 class RateReviewProvider with ChangeNotifier {
   bool _loading = false;
   bool get loading => _loading;
+
+  Future<Either<Failure, bool>> rateArtisan(
+      String serviceId, String artisanPhone, int rating,
+      [int jobId]) {}
 }
