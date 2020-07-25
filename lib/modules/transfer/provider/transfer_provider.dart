3c7cacc3764e1c06c132e2b68a65d3a3dafc1ba4
Charles Archibong <charlesarchibong10@gmail.com>
2020-07-25 20:14:58 +0100

    implement the get bank list in provider for proper state management

--- lib/modules/transfer/provider/transfer_provider.dart
+++ lib/modules/transfer/provider/transfer_provider.dart
@@ -1,7 +1,27 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:logger/logger.dart';
+import 'package:quickfix/models/failure.dart';
 import 'package:quickfix/modules/transfer/model/bank_list.dart';
+import 'package:quickfix/modules/transfer/service/transfer_api.dart';
 
 class TransferProvider with ChangeNotifier {
   List<BankList> _bankList = List();
   List<BankList> get bankList => _bankList;
+
+  Future<Either<Failure, List<BankList>>> fetchBanks() async {
+    try {
+      List<BankList> bankList = await TransferApi().getBankList();
+      return right(bankList);
+    } catch (e) {
+      Logger().e(
+        e.toString(),
+      );
+      return left(
+        Failure(
+          message: 'An error occured, please try again',
+        ),
+      );
+    }
+  }
 }
