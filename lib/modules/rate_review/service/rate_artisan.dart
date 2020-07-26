5708564205c742054cc065cc95881bf73fe78e36
Charles Archibong <charlesarchibong10@gmail.com>
2020-07-26 18:46:50 +0100

    updates

--- lib/modules/rate_review/service/rate_artisan.dart
+++ lib/modules/rate_review/service/rate_artisan.dart
@@ -39,7 +39,33 @@ class RateArtisanApi extends RateReview {
   }
 
   @override
-  Future<bool> reviewArtisan(RateArtisan rateArtisan) async {
-    return true;
+  Future<bool> reviewArtisan(RateArtisan rateArtisan, String review) async {
+    try {
+      User currentUser = await Utils.getUserSession();
+      String apiKey = await Utils.getApiKey();
+      String url = 'https://manager.fixme.ng/save-review';
+      Map<String, dynamic> body = rateArtisan.toMap();
+      body['reviewed_by'] = currentUser.phoneNumber;
+      body['review'] = review;
+      Map<String, String> headers = {'Authorization': 'Bearer $apiKey'};
+      Response response = await NetworkService().post(
+        url: url,
+        body: {},
+        queryParam: body,
+        headers: headers,
+        contentType: ContentType.JSON,
+      );
+      debugPrint(response.data.toString());
+      if (response.statusCode == 200 && response.data['reqRes'] == 'true') {
+        return true;
+      } else {
+        throw Exception('Request was not successful, please try again.');
+      }
+    } catch (e) {
+      if (e is DioError) {
+        debugPrint(e.response.data);
+      }
+      rethrow;
+    }
   }
 }
