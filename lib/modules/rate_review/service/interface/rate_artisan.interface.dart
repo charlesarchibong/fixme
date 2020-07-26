d6fbe62368fe766466d5f1d1fed2cebab8107a6c
Charles Archibong <charlesarchibong10@gmail.com>
2020-07-26 18:47:02 +0100

    added new field

--- lib/modules/rate_review/service/interface/rate_artisan.interface.dart
+++ lib/modules/rate_review/service/interface/rate_artisan.interface.dart
@@ -2,5 +2,5 @@ import 'package:quickfix/modules/rate_review/model/rate.dart';
 
 abstract class RateReview {
   Future<bool> rateArtisan(RateArtisan rateArtisan);
-  Future<bool> reviewArtisan(RateArtisan rateArtisan);
+  Future<bool> reviewArtisan(RateArtisan rateArtisan, String review);
 }
