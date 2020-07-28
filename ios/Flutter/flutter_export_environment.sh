6ad2a37b782f3c61906f928aa6d119ff9455d905
cyuket <cyriluket12@gmail.com>
2020-07-28 12:41:45 +0100

    Installing dependence

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/charlesarchibong/Android/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/charlesarchibong/AndroidStudioProjects/fixme"
+export "FLUTTER_ROOT=/home/cyuket/developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/cyuket/Documents/work/work-in-progress/fixme"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/home/charlesarchibong/Android/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/cyuket/developer/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
