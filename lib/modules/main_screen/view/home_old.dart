f7a231f7056d4307727cb25acc9f128ee0837cf8
Charles Archibong <charlesarchibong10@gmail.com>
2020-07-29 19:56:15 +0100

    fixed scrolling issue on smaller device

--- lib/modules/main_screen/view/home_old.dart
+++ lib/modules/main_screen/view/home_old.dart
@@ -120,224 +120,229 @@ class _HomeState extends State<HomeW>
       ),
     );
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
-        child: ListView(
-          children: <Widget>[
-            SizedBox(height: 10.0),
+      body: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 10.0),
 
-            //Slider Here
+              //Slider Here
 
-            // CarouselSlider(
-            //   options: CarouselOptions(
-            //     autoPlay: true,
-            //     viewportFraction: 1.0,
-            //     height: MediaQuery.of(context).size.height / 2.4,
-            //     onPageChanged: (index, reason) {
-            //       setState(() {
-            //         _current = index;
-            //       });
-            //     },
-            //   ),
-            //   items: map<Widget>(
-            //     technicians,
-            //     (index, i) {
-            //       Map food = technicians[index];
-            //       return SliderItem(
-            //         img: food['img'],
-            //         isFav: true,
-            //         name: food['name'],
-            //         rating: 5.0,
-            //         raters: 23,
-            //       );
-            //     },
-            //   ).toList(),
-            // ),
-            // SizedBox(height: 20.0),
+              // CarouselSlider(
+              //   options: CarouselOptions(
+              //     autoPlay: true,
+              //     viewportFraction: 1.0,
+              //     height: MediaQuery.of(context).size.height / 2.4,
+              //     onPageChanged: (index, reason) {
+              //       setState(() {
+              //         _current = index;
+              //       });
+              //     },
+              //   ),
+              //   items: map<Widget>(
+              //     technicians,
+              //     (index, i) {
+              //       Map food = technicians[index];
+              //       return SliderItem(
+              //         img: food['img'],
+              //         isFav: true,
+              //         name: food['name'],
+              //         rating: 5.0,
+              //         raters: 23,
+              //       );
+              //     },
+              //   ).toList(),
+              // ),
+              // SizedBox(height: 20.0),
 
-            // Text(
-            //   "Search Service or Business",
-            //   style: TextStyle(
-            //     fontSize: 19,
-            //     fontWeight: FontWeight.w800,
-            //   ),
-            // ),
-            // Container(
-            //   height: 65.0,
-            //   child: ListView.builder(
-            //     scrollDirection: Axis.horizontal,
-            //     shrinkWrap: true,
-            //     itemCount: categories == null ? 0 : categories.length,
-            //     itemBuilder: (BuildContext context, int index) {
-            //       Map cat = categories[index];
-            //       return HomeCategory(
-            //         icon: cat['icon'],
-            //         title: cat['name'],
-            //         items: cat['items'].toString(),
-            //         isHome: true,
-            //       );
-            //     },
-            //   ),
-            // ),
-            InkWell(
-              onTap: () {
-                Clipboard.setData(
-                  ClipboardData(
-                    text: accountNumber,
-                  ),
-                );
-                FlushBarCustomHelper.showInfoFlushbar(
-                  context,
-                  accountNumber,
-                  'Copied to clipboard',
-                );
-              },
-              child: FutureBuilder<SharedPreferences>(
-                  future: SharedPreferences.getInstance(),
-                  builder: (context, snapshot) {
-                    return FutureBuilder<User>(
-                        future: Utils.getUserSession(),
-                        builder: (context, usersnapshot) {
-                          return Card(
-                            elevation: 6.0,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(5.0),
-                                  ),
-                                ),
-                                child: RichText(
-                                  textAlign: TextAlign.center,
-                                  text: TextSpan(
-                                    text: 'Your account number is ',
-                                    style: TextStyle(
-                                      color: snapshot.hasData
-                                          ? snapshot.data.getString("theme") ==
-                                                  'light'
-                                              ? Colors.black
-                                              : Colors.white
-                                          : Colors.white,
-                                      fontSize: 17,
+              // Text(
+              //   "Search Service or Business",
+              //   style: TextStyle(
+              //     fontSize: 19,
+              //     fontWeight: FontWeight.w800,
+              //   ),
+              // ),
+              // Container(
+              //   height: 65.0,
+              //   child: ListView.builder(
+              //     scrollDirection: Axis.horizontal,
+              //     shrinkWrap: true,
+              //     itemCount: categories == null ? 0 : categories.length,
+              //     itemBuilder: (BuildContext context, int index) {
+              //       Map cat = categories[index];
+              //       return HomeCategory(
+              //         icon: cat['icon'],
+              //         title: cat['name'],
+              //         items: cat['items'].toString(),
+              //         isHome: true,
+              //       );
+              //     },
+              //   ),
+              // ),
+              InkWell(
+                onTap: () {
+                  Clipboard.setData(
+                    ClipboardData(
+                      text: accountNumber,
+                    ),
+                  );
+                  FlushBarCustomHelper.showInfoFlushbar(
+                    context,
+                    accountNumber,
+                    'Copied to clipboard',
+                  );
+                },
+                child: FutureBuilder<SharedPreferences>(
+                    future: SharedPreferences.getInstance(),
+                    builder: (context, snapshot) {
+                      return FutureBuilder<User>(
+                          future: Utils.getUserSession(),
+                          builder: (context, usersnapshot) {
+                            return Card(
+                              elevation: 6.0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(5.0),
                                     ),
-                                    children: <TextSpan>[
-                                      TextSpan(
-                                        text: usersnapshot.hasData
-                                            ? '${usersnapshot.data.accountNumber}'
-                                            : '',
-                                        style: TextStyle(
-                                          color: Theme.of(context).accentColor,
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                        recognizer: TapGestureRecognizer()
-                                          ..onTap = () {
-                                            Clipboard.setData(
-                                              ClipboardData(
-                                                text: usersnapshot
-                                                    .data.accountNumber,
-                                              ),
-                                            );
-                                            FlushBarCustomHelper
-                                                .showInfoFlushbar(
-                                              context,
-                                              usersnapshot.data.accountNumber,
-                                              'Copied to clipboard',
-                                            );
-                                          },
+                                  ),
+                                  child: RichText(
+                                    textAlign: TextAlign.center,
+                                    text: TextSpan(
+                                      text: 'Your account number is ',
+                                      style: TextStyle(
+                                        color: snapshot.hasData
+                                            ? snapshot.data
+                                                        .getString("theme") ==
+                                                    'light'
+                                                ? Colors.black
+                                                : Colors.white
+                                            : Colors.white,
+                                        fontSize: 17,
                                       ),
-                                      TextSpan(
-                                        text:
-                                            '. Bank Name is Providus Bank. To receive/send money simply transfer to this account.',
-                                        style: TextStyle(
-                                          // color: Theme.of(context).primaryColor,
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.normal,
+                                      children: <TextSpan>[
+                                        TextSpan(
+                                          text: usersnapshot.hasData
+                                              ? '${usersnapshot.data.accountNumber}'
+                                              : '',
+                                          style: TextStyle(
+                                            color:
+                                                Theme.of(context).accentColor,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          recognizer: TapGestureRecognizer()
+                                            ..onTap = () {
+                                              Clipboard.setData(
+                                                ClipboardData(
+                                                  text: usersnapshot
+                                                      .data.accountNumber,
+                                                ),
+                                              );
+                                              FlushBarCustomHelper
+                                                  .showInfoFlushbar(
+                                                context,
+                                                usersnapshot.data.accountNumber,
+                                                'Copied to clipboard',
+                                              );
+                                            },
                                         ),
-                                      ),
-                                    ],
+                                        TextSpan(
+                                          text:
+                                              '. Bank Name is Providus Bank. To receive/send money simply transfer to this account.',
+                                          style: TextStyle(
+                                            // color: Theme.of(context).primaryColor,
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          );
-                        });
-                  }),
-            ),
-            // SizedBox(
-            //   height: 6,
-            // ),
-            // InkWell(
-            //   onTap: () {
-            //     print('bby');
-            //   },
-            //   child: Card(
-            //     elevation: 6.0,
-            //     child: Container(
-            //       decoration: BoxDecoration(
-            //         color: Colors.white,
-            //         borderRadius: BorderRadius.all(
-            //           Radius.circular(5.0),
-            //         ),
-            //       ),
-            //       child: TextField(
-            //         style: TextStyle(
-            //           fontSize: 15.0,
-            //           color: Colors.black,
-            //         ),
-            //         decoration: InputDecoration(
-            //           contentPadding: EdgeInsets.all(10.0),
-            //           border: OutlineInputBorder(
-            //             borderRadius: BorderRadius.circular(5.0),
-            //             borderSide: BorderSide(
-            //               color: Colors.white,
-            //             ),
-            //           ),
-            //           enabledBorder: OutlineInputBorder(
-            //             borderSide: BorderSide(
-            //               color: Colors.white,
-            //             ),
-            //             borderRadius: BorderRadius.circular(5.0),
-            //           ),
-            //           hintText: "Search Service Providers or Businesses",
-            //           suffixIcon: IconButton(
-            //             onPressed: () {
-            //               // _searchArtisans(_searchControl.text);
-            //             },
-            //             icon: Icon(
-            //               Icons.search,
-            //               color: Colors.black,
-            //             ),
-            //           ),
-            //           hintStyle: TextStyle(
-            //             fontSize: 15.0,
-            //             color: Colors.black,
-            //           ),
-            //         ),
-            //         maxLines: 1,
-            //         controller: _searchControl,
-            //         onSubmitted: (val) {
-            //           // _searchArtisans(val);
-            //         },
-            //       ),
-            //     ),
-            //   ),
-            // ),
+                            );
+                          });
+                    }),
+              ),
+              // SizedBox(
+              //   height: 6,
+              // ),
+              // InkWell(
+              //   onTap: () {
+              //     print('bby');
+              //   },
+              //   child: Card(
+              //     elevation: 6.0,
+              //     child: Container(
+              //       decoration: BoxDecoration(
+              //         color: Colors.white,
+              //         borderRadius: BorderRadius.all(
+              //           Radius.circular(5.0),
+              //         ),
+              //       ),
+              //       child: TextField(
+              //         style: TextStyle(
+              //           fontSize: 15.0,
+              //           color: Colors.black,
+              //         ),
+              //         decoration: InputDecoration(
+              //           contentPadding: EdgeInsets.all(10.0),
+              //           border: OutlineInputBorder(
+              //             borderRadius: BorderRadius.circular(5.0),
+              //             borderSide: BorderSide(
+              //               color: Colors.white,
+              //             ),
+              //           ),
+              //           enabledBorder: OutlineInputBorder(
+              //             borderSide: BorderSide(
+              //               color: Colors.white,
+              //             ),
+              //             borderRadius: BorderRadius.circular(5.0),
+              //           ),
+              //           hintText: "Search Service Providers or Businesses",
+              //           suffixIcon: IconButton(
+              //             onPressed: () {
+              //               // _searchArtisans(_searchControl.text);
+              //             },
+              //             icon: Icon(
+              //               Icons.search,
+              //               color: Colors.black,
+              //             ),
+              //           ),
+              //           hintStyle: TextStyle(
+              //             fontSize: 15.0,
+              //             color: Colors.black,
+              //           ),
+              //         ),
+              //         maxLines: 1,
+              //         controller: _searchControl,
+              //         onSubmitted: (val) {
+              //           // _searchArtisans(val);
+              //         },
+              //       ),
+              //     ),
+              //   ),
+              // ),
 
-            SizedBox(height: 10.0),
+              SizedBox(height: 10.0),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Service Providers",
-                  style: TextStyle(
-                    fontSize: 19,
-                    fontWeight: FontWeight.w800,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Service Providers",
+                    style: TextStyle(
+                      fontSize: 19,
+                      fontWeight: FontWeight.w800,
+                    ),
                   ),
-                ),
 //                 FlatButton(
 //                   child: Text(
 //                     "voew more",
@@ -349,12 +354,13 @@ class _HomeState extends State<HomeW>
 //                   ),
 //                   onPressed: () {},
 //                 ),
-              ],
-            ),
-            SizedBox(height: 10.0),
-            _serviceProvidersAroundMe(),
-            SizedBox(height: 30),
-          ],
+                ],
+              ),
+              SizedBox(height: 10.0),
+              _serviceProvidersAroundMe(),
+              SizedBox(height: 30),
+            ],
+          ),
         ),
       ),
     );
