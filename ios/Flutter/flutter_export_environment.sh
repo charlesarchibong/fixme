1703f447fc8f86e63cbe06a82cba0ccdd9d181ca
charlesarchibong <charlesarchibong>
2020-09-16 07:26:39 +0100

    fixing iOS bugs

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,20 +2,15 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/macbook/Library/Android/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/macbook/Documents/Dev/Flutter/fixme"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/macbook/Documents/Dev/Flutter/fixme/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-<<<<<<< HEAD
 export "FLUTTER_FRAMEWORK_DIR=/Users/macbook/Library/Android/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.3.2"
-export "FLUTTER_BUILD_NUMBER=8"
-=======
-export "FLUTTER_FRAMEWORK_DIR=/home/charlesarchibong/Android/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.3.3"
 export "FLUTTER_BUILD_NUMBER=9"
->>>>>>> 8e92f98b116da785a52dbf49adf69844a890e5ca
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
+export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
 export "PACKAGE_CONFIG=.packages"
